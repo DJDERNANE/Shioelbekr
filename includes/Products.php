c6a5926a5a0ecb6b3bec1a801cfb7f9a3b6225a5
DJDERNANE <108133683+DJDERNANE@users.noreply.github.com>
2022-09-25 15:21:33 +0200

    Add files via upload

---
+++ includes/Products.php
@@ -0,0 +1,88 @@
+<div class="most_common">
+    <h1>Wekly Offers</h1>
+    <div class="content">
+    <div class="cart">
+        <img class="like" src="imgs/like.png" alt="">
+        <div>
+            <img class="product" src="imgs/product.png" alt="">
+        </div> 
+        <h3>Title</h3>
+        <p class="desc">descruption</p>
+        <p class="price">711 <span>DA</span></p>
+    </div>
+    <div class="cart">
+        <img class="like" src="imgs/like.png" alt="">
+        <div>
+            <img class="product" src="imgs/p5.png" alt="">
+        </div> 
+        <h3>Title</h3>
+        <p class="desc">descruption</p>
+        <p class="price">711 <span>DA</span></p>
+    </div>
+    <div class="cart">
+        <img class="like" src="imgs/like.png" alt="">
+        <div>
+            <img class="product" src="imgs/p2.png" alt="">
+        </div> 
+        <h3>Title</h3>
+        <p class="desc">descruption</p>
+        <p class="price">711 <span>DA</span></p>
+    </div>
+    <div class="cart">
+        <img class="like" src="imgs/like.png" alt="">
+        <div>
+            <img class="product" src="imgs/p4.png" alt="">
+        </div> 
+        <h3>Title</h3>
+        <p class="desc">descruption</p>
+        <p class="price">711 <span>DA</span></p>
+    </div>
+    
+    </div>
+</div> 
+
+<div class="most_common">
+    <h1>Most Common</h1>
+    <div class="content">
+    <div class="cart">
+        <img class="like" src="imgs/like.png" alt="">
+        <div>
+            <img class="product" src="imgs/product.png" alt="">
+        </div> 
+        <h3>Title</h3>
+        <p class="desc">descruption</p>
+        <p class="price">711 <span>DA</span></p>
+    </div>
+    <div class="cart">
+        <img class="like" src="imgs/like.png" alt="">
+        <div>
+            <img class="product" src="imgs/p1.png" alt="">
+        </div> 
+        <h3>Title</h3>
+        <p class="desc">descruption</p>
+        <p class="price">711 <span>DA</span></p>
+    </div>
+    <div class="cart">
+        <img class="like" src="imgs/like.png" alt="">
+        <div>
+            <img class="product" src="imgs/p2.png" alt="">
+        </div> 
+        <h3>Title</h3>
+        <p class="desc">descruption</p>
+        <p class="price">711 <span>DA</span></p>
+    </div>
+    <div class="cart">
+        <img class="like" src="imgs/like.png" alt="">
+        <div>
+            <img class="product" src="imgs/p3.png" alt="">
+        </div> 
+        <h3>Title</h3>
+        <p class="desc">descruption</p>
+        <p class="price">711 <span>DA</span></p>
+    </div>
+    
+    </div>
+</div> 
+<div class="pub">
+    <img src="imgs/pub.png" alt="">
+</div>
---
+++ includes/footer.php
@@ -0,0 +1,55 @@
+<div class="footer " id="footer">
+        <div class="title">
+            <h1 titre="contact">Contact Us</h1>
+        </div>
+        <div class="info">
+            <div class="contact">
+                <h3 titre="add">Address</h3>
+                <div class="location">
+                    <a href="https://www.google.com/maps/dir//kawa+service/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x128e537adc5cffe3:0x55a426564751fe42?sa=X&ved=2ahUKEwjloNK_-P35AhXChc4BHWJECaMQ9Rd6BAhREAU" target="_blank"> <i class="bi bi-geo-alt-fill"></i> Ru hamza 82 baraki 16027 , alger </a>
+                </div>
+                <div class="tlf">
+                    <a> <i class="bi bi-telephone-fill"></i> +213 552288157  </a>
+                </div>
+                <div class="email" >
+                    <a href="https://wa.me/message/CDWA3X4NTY5SN1" target="_blank"> <i class="bi bi-whatsapp"></i> WhatsApp</a>
+                </div>
+                <div class="email" >
+                    <a> <i class="bi bi-envelope-fill"></i>contact@kawa-service.com </a>
+                </div>
+            </div>
+            <div class="contact">
+                <h3 titre="follow">FOLLOW US</h3>
+                <div class="location">
+                    <a href="https://www.facebook.com/kawaservicenewpage" target="_blank"> <i class="bi bi-facebook"></i> Facebook </a>
+                </div>
+                <div class="tlf">
+                    <a href="https://www.instagram.com/kawa.service/" target="_blank"> <i class="bi bi-instagram"></i>Instgram </a>
+                </div>
+                <div class="email" >
+                    <a> <i class="bi bi-linkedin"></i> LinkedIn </a>
+                </div>
+            </div>
+            
+            <div class="contact">
+                <h3 titre="more">More</h3>
+                <div class="location">
+                    <a href="index.php#about" titre="abouttitle">About Us</a>
+                </div>
+                <div class="tlf">
+                    <a href="index.php#team" titre ="team">Team </a>
+                </div>
+                <div class="email" >
+                    <a href="index.php#services" titre="services"> Services</a>
+                </div>
+                <div class="email" >
+                    <a href="pages/politique.html" target="_blank" titre="privacy"> Privacy Policy</a>
+                </div>
+                <div class="email" >
+                    <a href="pages/help.php" target="_blank" titre="help"> How we can help you ?</a>
+                </div>
+            </div>
+        </div>
+</div>
+
+
